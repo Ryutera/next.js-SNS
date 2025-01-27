af3bf0900b27fb9a2ac71806ac95c53a54b22e13
Ryutera <kuya9823@gmail.com>
2025-01-27 00:01:37 +0000

    favorite function created

--- prisma/migrations/20250124221307_update_post_author_relation/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("clerkId") ON DELETE RESTRICT ON UPDATE CASCADE;
